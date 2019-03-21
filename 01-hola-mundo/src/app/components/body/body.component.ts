3bbc5fb6afea69abcc81ac32d99a19150dde3458
saulvegar <saul.vega@vasster.com>
2019-03-21 16:42:06 -0600

    firts commit

---
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html'  
+})
+
+export class BodyComponent {
+    
+  frase: any = {
+    mensaje: 'Un gran poder require una gran responsabilidad',
+    autor: 'Ben Parker'
+  };
+
+  mostrar = true;
+}
