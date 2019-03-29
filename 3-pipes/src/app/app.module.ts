e04d95bd210d6ab29cbdf78c268fdcfb7eb3889e
saulvegar <saul.vega@vasster.com>
2019-03-29 11:19:11 -0600

    Finalizada seccion de pipes

--- 3-pipes/src/app/app.module.ts
+++ 3-pipes/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
